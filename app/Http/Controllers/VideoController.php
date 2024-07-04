8f636d93d3162284121a956d7149c8c785102e69
peyman7585 <abbaszadeh.8575@gmail.com>
2024-07-04 15:00:24 +0330

    validate update request

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\storeVideoRequest;
+use App\Http\Requests\UpdateVideoRequest;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
@@ -37,7 +38,7 @@ class VideoController extends Controller
         return view('videos.edit',compact('video'));
     }
 
-    public function update(Request $request ,Video $video){
+    public function update(UpdateVideoRequest $request,Video $video){
 
         $video->update($request->all());
 
---
+++ app/Http/Requests/UpdateVideoRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateVideoRequest extends storeVideoRequest
+{
+
+    public function rules(): array
+    {
+        return array_merge(parent::rules(),[
+            'slug'=>['required',Rule::unique('videos')->ignore($this->video),'alpha_dash'],
+        ]);
+    }
+
+}
