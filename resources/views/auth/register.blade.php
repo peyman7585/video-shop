7d51a12e1010bd1172a4e11d7953d7b03100bf4e
peyman7585 <abbaszadeh.8575@gmail.com>
2024-07-19 23:02:09 +0330

    customize register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,54 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('auth-layout')
+@section('class-body','sing-up-page')
+@section('content')
+    <!--======= log_in_page =======-->
+    <div id="log-in" class="site-form log-in-form">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div id="log-in-head">
+            <h1>ثبت نام</h1>
+            <div id="logo"><a href="{{route('index')}}"><img src="img/logo.png" alt=""></a></div>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="form-output">
+            <x-validation-errors></x-validation-errors>
+            <form action="{{route('register.store')}}" method="POST">
+                @csrf
+                <div class="form-group label-floating">
+                    <label class="control-label">نام</label>
+                    <input class="form-control" name="name" placeholder="" type="text">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">ایمیل</label>
+                    <input class="form-control" name="email" placeholder="" type="email">
+                </div>
+                <div class="form-group label-floating">
+                    <label class="control-label">رمز عبور</label>
+                    <input class="form-control" name="password" placeholder="" type="password">
+                </div>
+
+                <div class="form-group label-floating">
+                    <label class="control-label">تأیید رمز عبور</label>
+                    <input class="form-control" name="password_confirmation" placeholder="" type="password">
+                </div>
+
+
+
+                <div class="remember">
+                    <div class="checkbox">
+                        <label>
+                            <input name="remember" type="checkbox">
+                        مرا به خاطر بسیار
+                        </label>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-lg btn-primary full-width">ثبت نام</button>
+
+                <div class="or"></div>
+
+                <p>شما یک حساب کاربری دارید؟ <a href="{{route('login.create')}}"> ورود!</a> </p>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+    <!--======= // log_in_page =======-->
+@endsection
